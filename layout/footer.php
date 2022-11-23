0885c4a8abd297b16e08ea813d143f04abb7f177
EzequielMor <elnaim2@gmail.com>
2022-11-23 17:34:00 -0300

    Footer mejorado v1

--- layout/footer.php
+++ layout/footer.php
@@ -3,23 +3,23 @@
   <img id="footer_logo" alt='imagen'  src="<?php echo $_SESSION['ruta']?>RECURSOS/logo.svg">
 
   <section>
+    <p class="titulofooter" >¡Contáctanos!</p><br>
     <p>Telefono: 4646 2902</p>
     <p><i class="fa-brands fa-whatsapp"></i>092 377 429</p>
-  </section>
-  <section>
-    <p>www.mimundo.com.uy</p>
     <p>mimundolibreria@gmail.com</p>
   </section>
-
-<p>Direccion: Acevedo Diaz esq. Francisco Canaro</p>
-
-
+  
+<section>
+  <p class="titulofooter" >Nuestra ubicacion.</p><br>
+  <p>www.mimundo.com.uy</p>
+  <p>Direccion: Acevedo Diaz esq. Francisco Canaro</p>
+</section>
 
 <div id="redes"> 
   <p>seguinos en nuestras redes!</p>
-  <i class="fa-brands fa-facebook"></i>
-  <i class="fa-brands fa-instagram"></i>
-  <i class="fa-brands fa-twitter"></i>
+  <i class="fa-brands fa-facebook">mimundo fb</i>
+  <i class="fa-brands fa-instagram">mimundo ig</i>
+  <i class="fa-brands fa-twitter">mimundo tw</i>
 </div>
 
 
