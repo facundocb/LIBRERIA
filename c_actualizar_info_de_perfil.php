43a6094e69c1ddcc29f7e56f16eed99b16ebda72
= <=>
2022-11-24 00:00:31 -0300

    cambiar info de perfil

---
+++ c_actualizar_info_de_perfil.php
@@ -0,0 +1,77 @@
+<?php
+
+
+session_start();
+
+include("functions/conexion.php");
+
+$user = $_SESSION['username'];
+$nom = $_POST['nombre'];
+$ape = $_POST['apellido'];
+$localidad = $_POST['localidad'];
+$fecha_nac = $_POST['fecha_nac'];
+$user_nuevo = $_POST['user'];
+$clave = $_POST['clave'];
+$conf_clave = $_POST['conf_clave'];
+
+
+
+if(strlen($nom) < 2 || preg_match("/[^[:alpha:]]/",$nom)){
+    $errors[0] ='El nombre no puede tener menos de dos caracteres, y no pueden tener caracteres numéricos o especiales';
+}
+
+if(strlen($ape) < 2 || preg_match("/[^[:alpha:]]/",$ape)){
+    $errors[1]= 'El apellido no puede tener menos de dos caracteres, y no pueden tener caracteres numéricos o especiales';
+    //apellido funciona igual que nombre, a mejorar: que se puedan usar espacios        
+}
+
+if(strlen($localidad) < 4 || preg_match("/[^[:word:]\s]/",$localidad)){
+    $errors[2]='La ciudad no puede tener caracteres especiales';
+     //esta vez uso word en vez de alpha por si le pinta poner un numero de calle o algo, igual no puede usar espacios asi que a mejorar
+}
+
+
+
+if($user_nuevo != $user){
+
+    if(strlen($user_nuevo) < 4 || preg_match("/\s/", $user_nuevo)){
+        $errors[4]='EL NOMBRE DE USUARIO no puede contener espacios ni ser inferior a 4 carácteres';
+    
+    }else{
+        if(consulta_usuario($user_nuevo)){
+            $errors[4]='el user ya está en uso, elegí otro';
+        }
+    }
+}
+
+
+if(strlen($clave) < 8 || preg_match("/\s/", $clave)){
+    $errors[5]='La contraseña no puede tener menos de 8 caracteres , y no puede tener espacios';
+
+}
+
+if(!$clave == $conf_clave){
+    $errors[6]='las contraseñas no coinciden';
+}
+
+
+if(isset($errors)){
+    $errors['estado'] = 0;
+    echo json_encode($errors);
+}else{
+
+    $ci = consulta_CI_con_user($user);
+
+
+    $fecha_nacimiento = $_POST['fecha_nac'];
+
+    $hashed_pass = password_hash($clave, PASSWORD_DEFAULT);
+
+    modificar_cliente( $ci[0], $nom, $ape, $localidad,$fecha_nacimiento, $user_nuevo, $hashed_pass);
+
+    $result['estado'] = 1;
+    $_SESSION['username'] = $user_nuevo;
+    echo json_encode($result);
+}
+
+?>
--- v_info_de_perfil.php
+++ v_info_de_perfil.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 
 session_start();
 
@@ -7,57 +7,80 @@ include("layout/header.php");
 ?>
 
 
-    <h1 class='titulo'>Mi cuenta</h1>
-    <div id="container_info_de_perfil">
-        
-        <div id="menu">
-            <a href="v_info_de_perfil.php"><h4 class='opcion selected'>Datos personales</h4></a>
-            <a href="v_historial_de_compras.php"><h4 class='opcion'>Historial de compras</h4></a>
-            <a href="<?php echo $_SESSION['ruta'] ?>/functions/c_cerrar_sesion.php"><h4 class='opcion'>Salir</h4></a>
-        </div>
-        
-        <div id='datos_personales'>
-            <div class="titulo_container">
-                <h2 class='titulo'>Datos</h2>
-                <div id="botones">
-                    <button id='edit' onclick="modificar_datos()"><span class="material-icons">edit</span></button>
-                    <button id="cancel" onclick="cancelar()"></button>
-                </div>
-            </div>
-                
-            <div class="content">
-                <p class="subtitulo">Nombre: </p>
-                <input type="text" disabled class="input_general" name='datos'>
-            </div>
-            <div class="content">
-                <p class="subtitulo">Apellido: </p>
-                <input type="text" disabled class="input_general" name='datos'>
-            </div>
-            <div class="content">
-                <p class="subtitulo">Localidad: </p>
-                <input type="text" disabled class="input_general" name='datos'>
-            </div>
-            <div class="content">
-                <p class="subtitulo">Cedula </p>
-                <input type="text" disabled class="input_general" name='datos'>
-            </div>
-            <div class="content">
-                <p class="subtitulo">Nacimiento: </p>
-                <input type="text" disabled class="input_general" name='datos'>
-            </div>
-            <div class="content">
-                <p class="subtitulo">Username: </p>
-                <input type="text" disabled class="input_general" name='datos'>
-            </div>
-            <div class="content">
-                <p class="subtitulo" id='pass'>clave: </p>
-                <input type="text" disabled class="input_general" name='datos' value='**********'>
-            </div>
+<h1 class='titulo'>Mi cuenta</h1>
+<div id="container_info_de_perfil">
 
+    <div id="menu">
+        <a href="v_info_de_perfil.php">
+            <h4 class='opcion selected'>Datos personales</h4>
+        </a>
+        <a href="v_historial_de_compras.php">
+            <h4 class='opcion'>Historial de compras</h4>
+        </a>
+        <a href="<?php echo $_SESSION['ruta'] ?>/functions/c_cerrar_sesion.php">
+            <h4 class='opcion'>Salir</h4>
+        </a>
+    </div>
+
+    <div id='datos_personales'>
+
+        <div id="alerta"></div>
+
+        <div class="titulo_container">
+            <h2 class='titulo'>Datos</h2>
+            <div id="botones">
+                <button id='edit' onclick="modificar_inputs()"><span class="material-icons">edit</span></button>
+
+            </div>
         </div>
+
+        <div class="content">
+            <p class="subtitulo">Nombre: </p>
+            <input type="text" disabled class="input_general" name='datos'>
+        </div>
+        <div class="error_upd"></div>
+
+
+
+        <div class="content">
+            <p class="subtitulo">Apellido: </p>
+            <input type="text" disabled class="input_general" name='datos'>
+        </div>
+        <div class="error_upd"></div>
+
+        <div class="content">
+            <p class="subtitulo">Localidad: </p>
+            <input type="text" disabled class="input_general" name='datos'>
+        </div>
+        <div class="error_upd"></div>
+
+        <div class="content">
+            <p class="subtitulo">Cedula </p>
+            <input type="text" disabled class="input_general" name='datos'>
+        </div>
+
+        <div class="content">
+            <p class="subtitulo">Nacimiento: </p>
+            <input type="text" disabled class="input_general" name='datos'>
+        </div>
+        <div class="error_upd"></div>
+
+        <div class="content">
+            <p class="subtitulo">Username: </p>
+            <input type="text" disabled class="input_general" name='datos'>
+        </div>
+        <div class="error_upd"></div>
+
+        <div class="content">
+            <p class="subtitulo" id='pass'>clave: </p>
+            <input type="text" disabled class="input_general" name='datos' value='**********'>
+        </div>
+        <div class="error_upd"></div>
 
     </div>
 
+</div>
 
-    <script src="<?php echo $_SESSION['ruta'] ?>/layout/script.js"></script>
-    <script src='info_de_perfil.js'></script>
+
+<script src="<?php echo $_SESSION['ruta'] ?>/layout/script.js"></script>
+<script src='info_de_perfil.js'></script>
